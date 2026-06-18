2d0c8b9875fcda5e79f781e42a2149d0060fde2d
Manuel Perez <m.perez@disacomp.com>
2026-06-18 17:30:21 -0400

    Refactor user management endpoints: improve error handling and response status codes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const getUserIdFromRequest = (req, res) => {
     const userId = Number(req.params.id);
     const user = users.find(user => user.id === userId)
     if (!user) {
-        res.json({
+        res.status(404).json({
             'message': `Usuario con el id ${req.params.id} no fue encontrado`
         })
         return null;
@@ -43,36 +43,61 @@ app.get('/users/:id', (req, res) => {
     res.json(user)
 })
 
-app.delete('/user/:id', (req, res) => {
+app.post('/users', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(400).json({
+            'message': 'El nombre es requerido'
+        })
+        return;
+    }
+    users.push({
+        id: users[users.length - 1].id + 1,
+        name
+    })
+    res.status(201).json({
+        message: `Se ha creado el usuario ${name} correctamente`,
+        res: users
+    })
+})
+
+app.put('/users/:id', (req, res) => {
+    const userObtainer = getUserIdFromRequest(req, res);
+    if (!userObtainer) return;
+    const index = users.findIndex(user => user.id === userObtainer.id);
+    if (!req.body?.name) {
+        res.status(400).json({
+            'message': 'No se encuentran propiedades name'
+        })
+        return;
+    }
+
+    if(!req.body?.name?.trim()){
+        res.status(406).json({
+            'message': 'La propiedad name no puede estar vacía'
+        })
+        return;
+    }
+    users[index].name = req.body.name;
+    res.status(200).json({
+        'message': 'Usuario modificado correctamente',
+        'res': users[index]
+    })
+
+})
+
+app.delete('/users/:id', (req, res) => {
     const userObtainer = getUserIdFromRequest(req, res);
     if (!userObtainer) return;
 
     const index = users.findIndex(user => user.id === userObtainer.id);
     if (index !== -1) {
         users.splice(index, 1)
-        res.json({
+        res.status(200).json({
             'message': `Se elimino el usuario ${userObtainer.name} correctamente`
         })
         return;
     }
-
-})
-
-app.put('/user/:id', (req, res) => {
-    const userObtainer = getUserIdFromRequest(req, res);
-    if (!userObtainer) return;
-    const index = users.findIndex(user => user.id === userObtainer.id);
-    if(!req.body){
-        res.json({
-            'message':'No se encuentran propiedades en el body'
-        })
-        return;
-    }
-    users[index].name = req.body.name;
-    res.json({
-        'message':'Usuario modificado correctamente',
-        'res': users[index]
-    })
 
 })
 
